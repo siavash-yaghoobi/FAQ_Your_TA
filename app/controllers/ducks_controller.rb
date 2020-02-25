e86c74be22decdfa815d77d3850dd990fb5cdb27
Adam Pozidis <adchrpoz@gmail.com>
2020-02-25 14:28:25 +0100

    some debugging

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -1,11 +1,18 @@
 class DucksController < ApplicationController
+  before_action :set_duck, only: %i[show edit destroy]
   def index
     @ducks = Duck.all
   end
 
+  def show
+  end
+
   def new
-    @user = User.find([params[:user_id]])
     @duck = Duck.new
+  end
+
+  def edit
+    @duck.update
   end
 
   def create
@@ -13,14 +20,22 @@ class DucksController < ApplicationController
     @duck.user = current_user
     @duck.save
     if @duck.save
-      redirect :duck_path
+      redirect_to duck_path(@duck)
     else
       render :new
     end
   end
 
+  def destroy
+    @duck.destroy
+  end
+
   private
   def duck_params
     params.require(:duck).permit(:name, :category, :description)
+  end
+
+  def set_duck
+    @duck = Duck.find(params[:id])
   end
 end
