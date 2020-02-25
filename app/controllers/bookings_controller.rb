6c28b5f676bd299cce86166de1ca94136ced4ea1
Adam Pozidis <adchrpoz@gmail.com>
2020-02-25 16:23:43 +0100

    test

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,18 +1,13 @@
 class BookingsController < ApplicationController
   before_action :set_booking, only: %i[edit update destroy]
-  def new
-    @user = User.find(params[:user_id])
-    @booking = Booking.new
-  end
 
   def create
-    user = User.find(params[:user_id])
-    duck = Duck.find(params[:duck_id])
-    booking = Booking.new(booking_params)
-    booking.user = user
-    booking.duck = duck
-    if booking.save
-      redirect_to user_path(user)
+    @duck = Duck.find(params[:duck_id])
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.duck = @duck
+    if @booking.save
+      redirect_to duck_path(@duck)
     else
       render :new
     end
@@ -21,7 +16,9 @@ class BookingsController < ApplicationController
     end
 
     def update
+      @duck = Duck.find(params[:duck_id])
       @booking.update
+      redirect_to duck_path(@duck)
     end
 
     def destroy
