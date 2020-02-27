bc2e8529c49584aeb4372bb3c04f7dac2e67543c
Siavash Yaghoobi <siavashy13@gmail.com>
2020-02-27 14:39:23 +0100

    working on index

--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,22 @@ ActiveRecord::Schema.define(version: 2020_02_27_100558) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
+  create_table "cocktails", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "doses", force: :cascade do |t|
+    t.text "description"
+    t.bigint "cocktail_id"
+    t.bigint "ingredient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
+    t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
+  end
+
   create_table "ducks", force: :cascade do |t|
     t.bigint "user_id"
     t.string "category"
@@ -56,6 +72,12 @@ ActiveRecord::Schema.define(version: 2020_02_27_100558) do
     t.float "price"
     t.string "address"
     t.index ["user_id"], name: "index_ducks_on_user_id"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -74,5 +96,7 @@ ActiveRecord::Schema.define(version: 2020_02_27_100558) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "ducks"
   add_foreign_key "bookings", "users"
+  add_foreign_key "doses", "cocktails"
+  add_foreign_key "doses", "ingredients"
   add_foreign_key "ducks", "users"
 end
