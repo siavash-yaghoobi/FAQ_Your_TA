707eab931411a02fb14ae818d44178b5121b48f5
Niccolo Manco <nickneuke@gmail.com>
2020-02-27 12:44:01 +0100

    search implemented

--- app/models/duck.rb
+++ app/models/duck.rb
@@ -1,5 +1,11 @@
 class Duck < ApplicationRecord
+  include PgSearch::Model
   has_one_attached :photo
   belongs_to :user
   has_many :bookings
+  pg_search_scope :search_by_name_category_description,
+    against: [ :name, :category, :description ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
