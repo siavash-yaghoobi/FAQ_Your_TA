00413833f4fe35d8087cea929e9ec048e60c7889
Niccolo Manco <nickneuke@gmail.com>
2020-02-27 18:41:48 +0100

    listings and autorizathion

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     resources :bookings, only: :create
   end
   resources :bookings, except: :create
+  get "listings", to: "ducks#listings", as: :listings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
