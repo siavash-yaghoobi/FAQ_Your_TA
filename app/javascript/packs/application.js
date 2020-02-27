186cb894d43f2d38ec05e943b0ce052f1a79d31e
Niccolo Manco <nickneuke@gmail.com>
2020-02-27 18:49:50 +0100

    listing resolved

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,11 +5,12 @@
 
 // initMapbox();
 import "bootstrap";
-import "../plugins/flatpickr"
+import "../plugins/flatpickr";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
+import "../booking_listener";
 initMapbox();
 initAutocomplete();
